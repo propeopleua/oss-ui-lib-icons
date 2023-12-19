9416c61fc664bdb1956cc691ea194994add1aec8
Andrii Muzyka <Andrii.Muzyka@pfizer.com>
2023-12-19 13:54:03 +0200

    - initial commit

---
+++ actions/generate-icons.mjs
@@ -0,0 +1,14 @@
+import {readFileSync, writeFileSync} from 'node:fs';
+import {resolve, dirname} from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const DIRNAME = dirname(fileURLToPath(import.meta.url));
+const FILE_PATH = resolve(DIRNAME, '..', 'style.css');
+const DEST_FILE_PATH = resolve(DIRNAME, '..', 'icons.js');
+
+const data = readFileSync(FILE_PATH, {encoding: 'utf-8'});
+
+const matches = Array.from(data.matchAll(/\.(hi-.+?):before/gis));
+const classes = matches.map(match => match[1]).filter(item => `${item || ''}`.length).map(cl => `"${cl}"`);
+classes.sort();
+writeFileSync(DEST_FILE_PATH, `export default [${classes.join(', ')}];`, {encoding: 'utf-8'})
